b9b6fd51fd65fd7816ddfc5f66177751e6f5daa0
Subham <subhamckp12@gmail.com>
2024-11-08 00:19:50 +0530

    WIP UI logic cleanup actually and model loading is still not working

--- Glitter/Sources/FileExplorer.cpp
+++ Glitter/Sources/FileExplorer.cpp
@@ -11,57 +11,54 @@ void ProjectAsset::RenderFileExplorer(
                 std::string currentPath,
                 std::vector<std::string>& fileNames)
 {
-    if (ImGui::Begin("File Dialog", &getUIState().showFileExplorerWindow))
+    for (const auto& entry : fs::directory_iterator(currentPath))
     {
+        fileNames.push_back(entry.path().string());
+    }
 
-        for (const auto& entry : fs::directory_iterator(currentPath))
+    // Display the current path
+    ImGui::Text("Current Path: %s", currentPath.c_str());
+    
+    if(ImGui::Button("Go to Root of project"))
+    {
+        currentPath = State::state->projectRootLocation;
+    }
+
+    if(State::state->errorStack.size() != 0)
+    ImGui::TextColored(ImVec4(1,0,0,1), State::state->errorStack.LastElement().c_str());
+
+    // Up button to go to the parent directory
+    if (ImGui::Button("Up"))
+    {
+        currentPath = fs::path(currentPath).parent_path().string();
+    }
+
+    // List files and directories
+    if (ImGui::BeginListBox("##FileList", ImVec2(-FLT_MIN, 100)))
+    {
+        for (unsigned int fileIndex = 0; fileIndex < fileNames.size(); fileIndex++)
         {
-            fileNames.push_back(entry.path().string());
-        }
-
-        // Display the current path
-        ImGui::Text("Current Path: %s", currentPath.c_str());
-        
-        if(ImGui::Button("Go to Root of project"))
-        {
-            currentPath = State::state->projectRootLocation;
-        }
-
-        if(State::state->errorStack.size() != 0)
-        ImGui::TextColored(ImVec4(1,0,0,1), State::state->errorStack.LastElement().c_str());
-
-        // Up button to go to the parent directory
-        if (ImGui::Button("Up"))
-        {
-            currentPath = fs::path(currentPath).parent_path().string();
-        }
-
-        // List files and directories
-        if (ImGui::BeginListBox("##FileList", ImVec2(-FLT_MIN, 100)))
-        {
-            for (unsigned int fileIndex = 0; fileIndex < fileNames.size(); fileIndex++)
+            const bool isSelected = (getUIState().selectedFileIndex == fileIndex);
+            if (ImGui::Selectable(fileNames[fileIndex].c_str(), isSelected))
             {
-                const bool isSelected = (getUIState().selectedFileIndex == fileIndex);
-                if (ImGui::Selectable(fileNames[fileIndex].c_str(), isSelected))
-                {
-                    getUIState().selectedFileIndex = fileIndex;
+                getUIState().selectedFileIndex = fileIndex;
 
-                    // If a directory is selected, navigate into it
-                    if (fs::is_directory(fileNames[fileIndex]))
-                    {
-                        currentPath = fileNames[fileIndex];
-                    }
+                // If a directory is selected, navigate into it
+                if (fs::is_directory(fileNames[fileIndex]))
+                {
+                    currentPath = fileNames[fileIndex];
                 }
             }
-            ImGui::EndListBox();
         }
-        ImGui::End();
+        ImGui::EndListBox();
     }
 }
 
 void ProjectAsset::saveAFile(std::string currentPath,
                 std::vector<std::string>& fileNames)
-{       
+{   
+    if(ImGui::Begin("FileExplorer"))
+    {
         ProjectAsset::RenderFileExplorer(currentPath, fileNames);
         switch(getUIState().fileTypeOperation)
         {
@@ -88,10 +85,14 @@ void ProjectAsset::saveAFile(std::string currentPath,
             }
             break;
         };
+    ImGui::End();
+    }
 }
 
 void ProjectAsset::selectOrLoadAFileFromFileExplorer(std::string currentPath,
                 std::vector<std::string>& fileNames){
+    if(ImGui::Begin("FileExplorer"))
+    {
         ProjectAsset::RenderFileExplorer(currentPath, fileNames);
 
         ImGui::Text("Selected File: %s", getUIState().filePath.c_str());
@@ -180,6 +181,8 @@ void ProjectAsset::selectOrLoadAFileFromFileExplorer(std::string currentPath,
                 }
             }
         }
+        ImGui::End();
+    }
 }
 
 bool ProjectAsset::InputText(const char* label, std::string& str, ImGuiInputTextFlags flags) {
---
+++ Glitter/Sources/outliner.cpp
@@ -0,0 +1,315 @@
+#include <UI/outliner.hpp>
+
+void Outliner::ModelMatrixComponent()
+{
+            if(getUIState().selectedModelIndex > -1)
+        {
+            glm::mat4& modelMatrix = (getUIState().models)[getUIState().selectedModelIndex]->model;
+            glm::vec3 scale, rotation, translation, skew;
+            glm::vec4 perspective;
+            glm::quat orientation;
+            
+            //Decompose the selected model matrix into translation, rotation and scale vectors
+            if (glm::decompose(modelMatrix, scale, orientation, translation, skew, perspective)) 
+            {
+                // Convert from radians to degrees if needed
+                rotation = glm::degrees(glm::eulerAngles(orientation));
+
+                ImGui::Text("Translation");
+                ImGui::DragFloat("X##translation", &translation.x, 0.005f);
+                ImGui::SameLine();
+                ImGui::DragFloat("Y##translation", &translation.y, 0.005f);
+                ImGui::SameLine();
+                ImGui::DragFloat("Z##translation", &translation.z, 0.005f);
+
+                // ImGui interface for scale
+                ImGui::Text("Scale");
+                ImGui::DragFloat("X##scale", &scale.x, 0.005f, 0.1f, 100.0f);  // Clamped to reasonable values
+                ImGui::SameLine();
+                ImGui::DragFloat("Y##scale", &scale.y, 0.005f, 0.1f, 100.0f);
+                ImGui::SameLine();
+                ImGui::DragFloat("Z##scale", &scale.z, 0.005f, 0.1f, 100.0f);
+
+                // // // ImGui interface for rotation
+                ImGui::Text("Rotation");
+                ImGui::DragFloat("X##rotation", &rotation.x, 0.1f);
+                ImGui::SameLine();
+                ImGui::DragFloat("Y##rotation", &rotation.y, 0.1f);
+                ImGui::SameLine();
+                ImGui::DragFloat("Z##rotation", &rotation.z, 0.1f);
+                
+                // glm::quat combinedRotation = orientation * newRotation;
+                modelMatrix = glm::recompose(scale, glm::quat(glm::radians(rotation)), translation, skew, perspective);
+
+            } else 
+            {
+                std::cerr << "Failed to decompose the the matrix" << std::endl;
+            }
+        }
+}
+void Outliner::levelControlsComponent(Level &lvl)
+{
+    ImGui::Text("Current Level %s", lvl.levelname.c_str());
+    if(ImGui::Button("Save Level"))
+    {
+        Level::saveToFile(lvl.levelname, lvl);
+    }
+    if(ImGui::Button("Save Level as"))
+    {
+        //POP up another window to give the lvl file a name and save perhaps ?
+        //give level a name
+        //choose the directory
+        getUIState().saveAsFileName = "";
+        getUIState().selectAFile = true;
+        getUIState().showOpenButton = false;
+        getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::saveLevelAs;
+    }
+    if(ImGui::Button("Load a level"))
+    {
+        getUIState().selectAFile = true;
+        getUIState().showOpenButton = true;
+        getUIState().fileExtension = ".lvl";
+        getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::LoadLvlFile;
+    }
+}
+void Outliner::modelSelectorComponent()
+{
+    for (int i = 0; i < getUIState().models.size(); ++i) {
+        // Optionally, push style changes here if you want to customize appearance
+        // Render the radio button
+        // The label for each button could be customized further if needed
+        std::string name = (getUIState().models)[i]->getName();
+        name+=std::to_string(i);
+        if (ImGui::RadioButton( name.c_str(), &getUIState().selectedModelIndex, i)) {
+        }
+        // Optionally, pop style changes here if you made any
+    }
+}
+void Outliner::coordinateSystemSelectorComponent()
+{
+    ImGui::Text("Coordinate space");
+    if (ImGui::RadioButton("World space",&getUIState().coordinateSpace,0)) {
+        State::state->isWorldSpace = true;
+    }
+    if (ImGui::RadioButton("Local space",&getUIState().coordinateSpace,1)) {
+        State::state->isWorldSpace = true;
+    }
+}
+void Outliner::manageModels()
+{
+    if(ImGui::Button("Import a Model"))
+    {
+        getUIState().loadModelWindow = true;
+    }
+    if(ImGui::Button("Save a Model"))
+    {
+        //Get selected index of the model
+        //open UI to enter name of the model
+        //call saveModel function
+        getUIState().selectAFile = true;
+        getUIState().showOpenButton = false;
+        getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::saveModel;
+    }
+    if(ImGui::Button("Load a Model"))
+    {
+        getUIState().selectAFile = true;
+        getUIState().showOpenButton = true;
+        getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::loadModel;
+    }
+}
+void Outliner::manageAnimationsForSelectedModel()
+{
+    if(ImGui::Button("Load animation for model"))
+    {
+        getUIState().selectAFile = true;
+        getUIState().showOpenButton = true;
+        getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::loadAnimation;
+    }
+    if (ImGui::Combo("Choose an option", &getUIState().selectedAnimationIndex, getUIState().animationNames.data(),getUIState().animationNames.size())) {
+        // Action when the selection changes
+        // std::cout << "Selected: " << animationNames[current_item] << std::endl;
+    }
+    if(ImGui::Button("Play AnimationType"))
+    {
+        if(getUIState().character->animator != nullptr)
+        getUIState().character->animator->PlayAnimation(getUIState().animations[getUIState().selectedAnimationIndex]);
+        // we need to send the manipulation to mesh
+    }
+}
+void Outliner::popupForErrorsAndWarning()
+{
+    if (ImGui::BeginPopupModal("Warning", NULL, ImGuiWindowFlags_AlwaysAutoResize)) {
+        ImGui::Text(State::state->errorStack.LastElement().c_str());
+        ImGui::Separator();
+
+        if (ImGui::Button("OK")) { 
+            State::state->errorStack.LastElement() = "";
+            ImGui::CloseCurrentPopup();
+        }
+        ImGui::EndPopup();
+    }
+    if(State::state->errorStack.size() != 0)
+    ImGui::OpenPopup("Warning");
+}
+void Outliner::debugOptions()
+{
+    if(ImGui::Button("Increment selected bone"))
+    {
+        if(getUIState().character != nullptr)
+        {
+            getUIState().selectedBoneId++;
+        }
+    }
+    if(ImGui::Button("Reset selected bone"))
+    {
+        if(getUIState().character != nullptr)
+        {
+            getUIState().selectedBoneId = 0;
+        }
+    }
+}
+
+void Outliner::handlerForUIComponentsvisibility()
+{
+    if(getUIState().selectAFile)
+    {
+        ProjectAsset::selectOrLoadAFileFromFileExplorer(fs::current_path().string(), State::state->uiState.fileNames);
+    }
+    else if(getUIState().saveAFile){
+        ProjectAsset::saveAFile(fs::current_path().string(), State::state->uiState.fileNames);
+    }
+
+    if(getUIState().loadModelWindow)
+    {
+        ModelAndTextureSelectionWindow();
+    }
+}
+void Outliner::ModelAndTextureSelectionWindow()
+{
+    if(ImGui::Begin("Import ModelType", &getUIState().loadModelWindow))
+    {   
+        ImGui::Text("ModelType");
+        ImGui::SameLine();
+        ImGui::Text("%s", getUIState().modelfileName.c_str());
+        ImGui::SameLine();
+        if(ImGui::Button("Browse##1"))
+        {
+            getUIState().selectAFile = true;
+            getUIState().showOpenButton = true;
+            getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::importModelFile;
+        }
+
+        ImGui::Text("Albedo");
+        ImGui::SameLine();
+        ImGui::Text("%s", getUIState().albedo.c_str());
+        ImGui::SameLine();
+        if(ImGui::Button("Browse##2"))
+        {
+            getUIState().selectAFile = true;
+            getUIState().showOpenButton = true;
+            getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::albedoTexture;
+        }
+
+        ImGui::Text("Normal");
+        ImGui::SameLine();
+        ImGui::Text("%s", getUIState().normal.c_str());
+        ImGui::SameLine();
+        if(ImGui::Button("Browse##3"))
+        {
+            getUIState().selectAFile = true;
+            getUIState().showOpenButton = true;
+            getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::normalTexture;
+        }
+
+        ImGui::Text("Metalness");
+        ImGui::SameLine();
+        ImGui::Text("%s", getUIState().metalness.c_str());
+        ImGui::SameLine();
+        if(ImGui::Button("Browse##5"))
+        {
+            getUIState().selectAFile = true;
+            getUIState().showOpenButton = true;
+            getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::metalnessTexture;
+        }
+
+        ImGui::Text("Roughness");
+        ImGui::SameLine();
+        ImGui::Text("%s", getUIState().roughness.c_str());
+        ImGui::SameLine();
+        if(ImGui::Button("Browse##6"))
+        {
+            getUIState().selectAFile = true;
+            getUIState().showOpenButton = true;
+            getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::roughnessTexture;
+        }
+
+        ImGui::Text("AO");
+        ImGui::SameLine();
+        ImGui::Text("%s", getUIState().ao.c_str());
+        ImGui::SameLine();
+        if(ImGui::Button("Browse##7"))
+        {
+            getUIState().selectAFile = true;
+            getUIState().showOpenButton = true;
+            getUIState().fileTypeOperation = ProjectAsset::FileTypeOperation::aoTexture;
+        }
+
+        ImGui::End();
+    }
+}
+
+void Outliner::applyRotation(glm::mat4& modelMatrix, glm::vec3 rotationDegrees, bool isLocal)
+{
+    // Convert degrees to radians for rotation
+    glm::vec3 rotationRadians = glm::radians(rotationDegrees);
+
+    // Prepare rotation quaternions based on the input
+    glm::quat quatX = glm::angleAxis(rotationRadians.x, glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::quat quatY = glm::angleAxis(rotationRadians.y, glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::quat quatZ = glm::angleAxis(rotationRadians.z, glm::vec3(0.0f, 0.0f, 1.0f));
+
+    // Combine rotations
+    glm::quat combinedQuat = quatZ * quatY * quatX;
+
+    // Calculate rotation matrix
+    glm::mat4 rotationMatrix = glm::mat4_cast(combinedQuat);
+
+    // Apply rotation
+    if (isLocal) {
+        // Local rotation
+        modelMatrix *= rotationMatrix;
+    } else {
+        // World rotation: rotate around global axes
+        // Reset the translation part to avoid moving the object
+        glm::vec3 translation = glm::vec3(modelMatrix[3]);
+        modelMatrix[3] = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+        modelMatrix = rotationMatrix * modelMatrix;
+        modelMatrix[3] = glm::vec4(translation, 1.0f);
+    }
+}
+
+void Outliner::updateAnimator(float deltatime)
+{
+    if(getUIState().selectedAnimationIndex != -1 && getUIState().character != nullptr && getUIState().animations.size() != 0)
+    getUIState().character->animator->UpdateAnimation(deltatime);
+}
+
+void Outliner::updateFinalBoneMatrix(Shader ourShader)
+{
+    getUIState().shaderOfSelectedModel = &ourShader;
+    if(getUIState().character->animator != nullptr)
+    {
+        auto transforms = getUIState().character->animator->GetFinalBoneMatrices();
+        for (int i = 0; i < transforms.size(); ++i)
+            ourShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transforms[i]);
+    }
+    else{
+        //MAXBONES 100
+            for (int i = 0; i < 100; ++i)
+            ourShader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", 1.0f);
+    }
+    if(getUIState().character->model != nullptr)
+    {
+        getUIState().shaderOfSelectedModel->setInt("displayBoneIndex", getUIState().selectedBoneId);
+    }
+}
